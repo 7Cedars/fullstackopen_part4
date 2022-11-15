1a054bc70635130a9ceff489f2c3bb54da2737d7
7Cedars <Cedars7@proton.me>
2022-11-15 14:28:53 +0000

    e4.5

--- blogs-app-backend/tests/list_helper.test.js
+++ blogs-app-backend/tests/list_helper.test.js
@@ -1,61 +1,59 @@
 const listHelper = require('../utils/list_helper')
+const listWithNoBlog = []
+
+const listWithOneBlog = [
+    {
+      _id: 'Aa422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+const listWithMultipleBlogs = [
+    {
+      _id: 'Ba422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful Really?',
+      author: 'Some Author',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    }, 
+    {
+        _id: 'Ca422aa71b54a676234d17f8',
+        title: 'Ok then',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 15,
+        __v: 0
+      },
+    {
+      _id: 'Da422aa71b54a676234d17f8',
+      title: 'But what will I find?',
+      author: 'Who Is Real',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: 'Ea422aa71b54a676234d17f8',
+      title: '... hello?',
+      author: 'Jane Doe',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 12,
+      __v: 0
+    }, 
+  ]
 
 test('dummy returns one', () => {
   const blogs = []
-
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
 
 describe('sum of likes', () => {
-    const listWithNoBlog = []
-
-    const listWithOneBlog = [
-        {
-          _id: 'Aa422aa71b54a676234d17f8',
-          title: 'Go To Statement Considered Harmful',
-          author: 'Edsger W. Dijkstra',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 5,
-          __v: 0
-        }
-      ]
-
-    const listWithMultipleBlogs = [
-        {
-          _id: 'Ba422aa71b54a676234d17f8',
-          title: 'Go To Statement Considered Harmful Really?',
-          author: 'Some Author',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 7,
-          __v: 0
-        }, 
-        {
-            _id: 'Ca422aa71b54a676234d17f8',
-            title: 'Ok then',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 15,
-            __v: 0
-          },
-        {
-          _id: 'Da422aa71b54a676234d17f8',
-          title: 'But what will I find?',
-          author: 'Who Is Real',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 2,
-          __v: 0
-        },
-        {
-          _id: 'Ea422aa71b54a676234d17f8',
-          title: '... hello?',
-          author: 'Jane Doe',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 12,
-          __v: 0
-        }, 
-      ]
-
     test('of empty list is zero', () => {
       const result = listHelper.totalLikes(listWithNoBlog)
       expect(result).toBe(0)
@@ -71,3 +69,53 @@ describe('sum of likes', () => {
       expect(result).toBe(36)
     })
   })
+
+  describe('max likes', () => {
+    test('of empty list is null', () => {
+      const result = listHelper.favoriteBlog(listWithNoBlog)
+      expect(result).toBe(null)
+    })
+  
+    test('if only one post, its max likes equals likes of blog post', () => {
+      const result = listHelper.favoriteBlog(listWithOneBlog)
+      console.log(result)
+      expect(result.likes).toBe(5)
+    })
+  
+    test('larger list is calculated correctly', () => {
+      const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+      expect(result.likes).toBe(15)
+    })
+  })
+
+  describe('favorite blog post', () => {
+    test('of empty list is null', () => {
+      const result = listHelper.favoriteBlog(listWithNoBlog)
+      expect(result).toEqual(null)
+    })
+  
+    test('of one post, is that post', () => {
+      const result = listHelper.favoriteBlog(listWithOneBlog)
+      console.log(result)
+      expect(result).toEqual(
+        {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        }
+      )
+    })
+  
+    test('of larger list is picked correctly', () => {
+      const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+      expect(result).toEqual(
+        {
+            title: 'Ok then',
+            author: 'Edsger W. Dijkstra',
+            likes: 15
+        }
+    )
+  })
+})
+
+ 
--- blogs-app-backend/utils/list_helper.js
+++ blogs-app-backend/utils/list_helper.js
@@ -13,9 +13,26 @@ const totalLikes = (blogs) => {
   return blogs.length === 0
   ? 0
   : blogs.reduce(summingLikes, 0) 
+}
 
+const favoriteBlog  = (blogs) => {
+
+  const maxLikes = (max, blog) => {
+    return Math.max(max, blog.likes)
+  }
+
+  const maxLiked = blogs.reduce(maxLikes, 0) 
+  const favBlog = blogs.find(blog => blog.likes === maxLiked)
+
+  return blogs.length === 0
+  ? null
+  : {
+      title: favBlog.title, 
+      author: favBlog.author, 
+      likes: favBlog.likes, 
+    }
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
