c12ad97b529c2d1ad3dfd566c6f0c1b62f6be4c0
7Cedars <Cedars7@proton.me>
2022-11-16 11:12:06 +0000

    e4.10

--- blogs-app-backend/tests/blog_api.test.js
+++ blogs-app-backend/tests/blog_api.test.js
@@ -23,12 +23,31 @@ test('correct number of blogs are returned', async () => {
 })
 
 test('unique identifier is named "id" (and not "_id")', async () => {
+    
     const response = await api.get('/api/blogs')
     console.log(Object.keys(response.body[0]))
 
     expect(response.body).toBeDefined()
     expect(Object.keys(response.body[0])).toContain('id')
     expect(Object.keys(response.body[0])).not.toContain('_id')
+})
 
+test('it saves a (valid) blog correctly', async () => {
+    const newSavedBlog =   {
+        title: "An added blog",
+        author: "Matti Luukkainen",
+        url: "How to Make create good MOOCs.",
+        likes: 15
+      }
+
+    await api
+        .post('/api/blogs')
+        .send(newSavedBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.firstBlogs.length + 1) 
 })
 
