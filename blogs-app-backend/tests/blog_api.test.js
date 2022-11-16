66e7186253b25aac0ee1dbf16092a2be0ae078bb
7Cedars <Cedars7@proton.me>
2022-11-16 11:04:31 +0000

    e4.9

--- blogs-app-backend/tests/blog_api.test.js
+++ blogs-app-backend/tests/blog_api.test.js
@@ -20,4 +20,15 @@ test('correct number of blogs are returned', async () => {
     const response = await api.get('/api/blogs')
   
     expect(response.body).toHaveLength(helper.firstBlogs.length)
-  })
+})
+
+test('unique identifier is named "id" (and not "_id")', async () => {
+    const response = await api.get('/api/blogs')
+    console.log(Object.keys(response.body[0]))
+
+    expect(response.body).toBeDefined()
+    expect(Object.keys(response.body[0])).toContain('id')
+    expect(Object.keys(response.body[0])).not.toContain('_id')
+
+})
+
